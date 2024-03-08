0c1aa2e5b44462cdc1dc94179e89c1177e4bf6a8
ChenXin(Zachary) Huang <140463149+ZacharyHuang1@users.noreply.github.com>
2024-03-08 12:06:19 +1030

    Add files via upload

---
+++ main-1-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+extern int sum_diagonal(int array[4][4]);
+int main() {
+  int matrix[4][4] = {
+    {1, 2, 3, 4},
+    {5, 6, 7, 8},
+    {9, 10, 11, 12},
+    {13, 14, 15, 16}
+  };
+  int result = sum_diagonal(matrix);
+  std::cout << "The sum of the main diagonal is " << result << std::endl;
+  return 0;
+}
---
+++ main-1-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+extern int is_identity(int array[10][10]);
+int main() {
+  int matrix[10][10] = {
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 1}
+  };
+  int result = is_identity(matrix);
+  std::cout << "The matrix is " << (result == 1 ? "an identity matrix" : "not an identity matrix") << std::endl;
+  return 0;
+}
